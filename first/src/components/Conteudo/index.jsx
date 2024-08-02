c8cf68384c8d0e0565c7c2bba18b3fbbf41d104d
GISELE MARA DOS ANJOS <gisele_anjos@estudante.sesisenai.org.br>
2024-08-01 22:01:23 -0300

    First

---
+++ first/src/components/Conteudo/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Conteudo() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem ullam laborum, molestias, asperiores magni temporibus amet voluptate minus nobis officia, ut omnis iusto quidem sapiente perferendis id necessitatibus aspernatur sequi.</p>
+    </div>
+  )
+}
+
+export default Conteudo
---
+++ first/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header-container">
+          <h1> This is my first React project</h1>
+    </div>
+  )
+}
+
+export default Header
